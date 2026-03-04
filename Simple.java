e89b03d53a531d123e936ae4cff71ec723763833
Sindhuraavi2023 <sindhuraavi14@gmail.com>
2026-03-04 11:15:48 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,7 +1,8 @@
 public class Simple{
   public static void main(String[] agrs){
-    int a=12;
+    int a=12 , int e=67;
     int b=34;
+    System.out.println(""+c);
   System.out.println(""+a);
   }
 }
